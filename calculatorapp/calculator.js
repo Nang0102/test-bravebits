9df57bf3842359dcf4747593dca7527377c72375
Mai Mai <ntmainbm6328@gmail.com>
2023-03-15 21:17:39 +0700

    done app

--- calculatorapp/calculator.js
+++ calculatorapp/calculator.js
@@ -12,10 +12,17 @@ keys.addEventListener("click", (e) => {
     const calculate = (n1, operator, n2) => {
       const firstNum = parseFloat(n1);
       const secondNum = parseFloat(n2);
+
       if (operator === "add") return firstNum + secondNum;
       if (operator === "subtract") return firstNum - secondNum;
       if (operator === "multiply") return firstNum * secondNum;
-      if (operator === "divide") return firstNum / secondNum;
+      if (operator === "divide") {
+        if (secondNum === 0) {
+          return 0;
+        } else {
+          return firstNum / secondNum;
+        }
+      }
     };
     if (!action) {
       if (displayNum === "0" || previousKeyType === "operator") {
@@ -34,14 +41,15 @@ keys.addEventListener("click", (e) => {
       const firstValue = calculator.dataset.firstValue;
       const operator = calculator.dataset.operator;
       const secondValue = displayNum;
+
       if (firstValue && operator !== "operator") {
         const calcValue = calculate(firstValue, operator, secondValue);
         display.textContent = calcValue;
-        calculator.dataset.firstValue = calcValue; // cap nhat lai gtri dau tien
+        calculator.dataset.firstValue = calcValue;
       } else {
         calculator.dataset.firstValue = displayNum;
       }
-      calculator.dataset.previousKeyType = "operator"; //goi phim da nhap truoc do la 1 toan tu(operator)
+      calculator.dataset.previousKeyType = "operator";
       calculator.dataset.operator = action;
     }
     if (action === "decimal") {
@@ -50,7 +58,7 @@ keys.addEventListener("click", (e) => {
       } else if (previousKeyType === "operator") {
         display.textContent = "0.";
       }
-      // cập nhật previousKeyType cho phím được nhập
+
       calculator.dataset.previousKeyType = "decimal";
     }
     if (action === "clear") {
