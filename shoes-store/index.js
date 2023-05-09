742b6885cdfe27a4421e74be708011fd310b08e6
Mai Mai <ntmainbm6328@gmail.com>
2023-05-10 00:44:37 +0700

    responsive header

---
+++ shoes-store/index.js
@@ -0,0 +1,21 @@
+// Đường dẫn của tệp header.html và footer.html
+const headerPath = "./header/Header.html";
+const contentPath = "./shoes-container/index.html";
+
+// Lấy phần tử chứa header và footer trong tệp index.html
+const headerElement = document.getElementById("header");
+const contentElement = document.getElementById("content");
+
+// Đọc nội dung từ tệp header.html và gán vào phần tử header
+fetch(headerPath)
+  .then((response) => response.text())
+  .then((content) => {
+    headerElement.innerHTML = content;
+  });
+
+// Đọc nội dung từ tệp footer.html và gán vào phần tử footer
+fetch(contentPath)
+  .then((response) => response.text())
+  .then((content) => {
+    contentElement.innerHTML = content;
+  });
