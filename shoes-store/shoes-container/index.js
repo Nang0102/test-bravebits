0f7ee99f2200390f38b999636f6e72118fbe2c5d
Nang0102 <ntmainbm6328@gmail.com>
2023-05-09 18:11:24 +0700

    create sale,craft

---
+++ shoes-store/shoes-container/index.js
@@ -0,0 +1,28 @@
+const DataShoes = [
+  {
+    img: "../image/shoe-1.png",
+    price: "460.9",
+    name: "Saint Laurent",
+    desc: "Wyatt Leather Chelsea Boots",
+  },
+  {
+    img: "../image/shoe-2.png",
+    price: "625.99",
+    name: "Brunello Cucinelli",
+    desc: "Burnished-Leather Oxford Shoes",
+  },
+  {
+    img: "../image/shoe-3.png",
+    price: "498.99",
+    initalPrice: "638.99",
+    name: "Gianvito Rossi",
+    desc: "Charles Leather Oxford Shoes",
+  },
+  {
+    img: "../image/shoe-4.png",
+    price: "460.9",
+    priceOff: "592.99",
+    name: "Loro Piana",
+    desc: "Pamington Open Walk Boots",
+  },
+];
