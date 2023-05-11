0d3995e7d85fa37ac16034b4b2790bc2619bd61a
Nang0102 <ntmainbm6328@gmail.com>
2023-05-11 18:07:21 +0700

    update

--- shoes-store/shoes-container/index.js
+++ shoes-store/shoes-container/index.js
@@ -1,28 +1,27 @@
-const DataShoes = [
-  {
-    img: "../image/shoe-1.png",
-    price: "460.9",
-    name: "Saint Laurent",
-    desc: "Wyatt Leather Chelsea Boots",
-  },
-  {
-    img: "../image/shoe-2.png",
-    price: "625.99",
-    name: "Brunello Cucinelli",
-    desc: "Burnished-Leather Oxford Shoes",
-  },
-  {
-    img: "../image/shoe-3.png",
-    price: "498.99",
-    initalPrice: "638.99",
-    name: "Gianvito Rossi",
-    desc: "Charles Leather Oxford Shoes",
-  },
-  {
-    img: "../image/shoe-4.png",
-    price: "460.9",
-    priceOff: "592.99",
-    name: "Loro Piana",
-    desc: "Pamington Open Walk Boots",
-  },
-];
+function changeLargeImage(image) {
+  const newSource = image.src;
+  const largeImage = document.getElementById("large-image");
+  largeImage.src = newSource;
+}
+
+// Đường dẫn của tệp header.html và footer.html
+const investmentPath = "../investment/investment.html";
+const contentPath = "./shoes-container/index.html";
+
+// Lấy phần tử chứa header và footer trong tệp index.html
+const investmentElement = document.getElementById("investment");
+const contentElement = document.getElementById("content");
+
+// Đọc nội dung từ tệp header.html và gán vào phần tử header
+fetch(investmentPath)
+  .then((response) => response.text())
+  .then((content) => {
+    investmentElement.innerHTML = content;
+  });
+
+// Đọc nội dung từ tệp footer.html và gán vào phần tử footer
+fetch(contentPath)
+  .then((response) => response.text())
+  .then((content) => {
+    contentElement.innerHTML = content;
+  });
